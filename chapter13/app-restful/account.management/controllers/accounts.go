7b3e0401b3030ea2f17bf4cd81f550b57b63c923
Sammy <xiangmin.li@qq.com>
2019-03-02 15:50:23 +0800

    feature(chapter13): LISTING 13‐14: Account Method That Generates Account Resources

--- chapter13/app-restful/account.management/controllers/accounts.go
+++ chapter13/app-restful/account.management/controllers/accounts.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
 
 	restful "github.com/emicklei/go-restful"
@@ -9,30 +10,39 @@ import (
 )
 
 const (
-	entryPointBaseURL = "http://localhost:4100/"
-	accountsBaseURL   = "http://localhost:4101/accountmanagement/"
+	entryPointBaseURL         = "http://localhost:4100/"
+	accountsManagementBaseURL = "http://localhost:4100/accountmanagement"
+	accountsBaseURL           = accountsManagementBaseURL + "/accounts"
 )
 
-func Index(req *restful.Request, resp *restful.Response) {
-	root := hal.NewResource(struct{}{}, accountsBaseURL+"accounts")
-	root.AddLink("alternative", hal.NewLink(accountsBaseURL+"accounts?page=1"))
-	root.AddLink("account", hal.NewLink(accountsBaseURL+"accounts/{accountID}"))
-	root.AddLink("account", hal.NewLink(accountsBaseURL+"accounts/123"))
-	root.AddLink("account", hal.NewLink(accountsBaseURL+"accounts/456"))
-	root.AddLink("next", hal.NewLink(accountsBaseURL+"accounts?page=2"))
-	root.AddLink("parent", hal.NewLink(entryPointBaseURL))
+type Item struct {
+	ID   string
+	Name string
+}
 
-	resp.WriteHeaderAndJson(http.StatusOK, root, "application/hal+json")
+func Index(req *restful.Request, resp *restful.Response) {
+	root := hal.NewResource(struct{}{}, accountsBaseURL)
+	root.AddLink("alternative", hal.NewLink(accountsBaseURL+"accounts?page=1"))
+	root.AddLink("account", hal.NewLink(accountsBaseURL+"/{accountID}"))
+	root.AddLink("account", hal.NewLink(accountsBaseURL+"/123"))
+	root.AddLink("account", hal.NewLink(accountsBaseURL+"/456"))
+	root.AddLink("next", hal.NewLink(accountsBaseURL+"?page=2"))
+	root.AddLink("parent", hal.NewLink(accountsManagementBaseURL))
+
+	resp.WriteHeaderAndJson(http.StatusOK, root, conf.MIME_HAL_JSON)
 }
 
 func Account(req *restful.Request, resp *restful.Response) {
-	resp.Header().Set("Content-Type", conf.MIME_HAL_JSON)
+	id := req.PathParameter("accountID")
 
-	/*
-		root := hal.NewResource(struct{}{}, entryPointBaseURL+"/accountmanagement")
-		root.AddLink("accounts", hal.NewLink(accountsBaseURL+"/accountmanagement/accounts"))
+	root := hal.NewResource(
+		Item{ID: id, Name: "Account_" + id},
+		accountsBaseURL+"/"+id)
 
-		data, _ := json.Marshal(root)
-		resp.Write(data)
-	*/
+	root.AddLink("collection", hal.NewLink(accountsBaseURL))
+	root.AddLink("followers", hal.NewLink(fmt.Sprintf("%s/%s/followers", accountsBaseURL, id)))
+	root.AddLink("following", hal.NewLink(fmt.Sprintf("%s/%s/following", accountsBaseURL, id)))
+	root.AddLink("blurbs", hal.NewLink(fmt.Sprintf("%s/%s/blurbs", accountsBaseURL, id)))
+
+	resp.WriteHeaderAndJson(http.StatusOK, root, conf.MIME_HAL_JSON)
 }
