297fa21e173514ae788b5432c2d6125967478173
Sammy <xiangmin.li@qq.com>
2019-03-02 15:17:30 +0800

    feature(chapter13): add the Index() handler for accounts API

--- chapter13/app-restful/account.management/controllers/accounts.go
+++ chapter13/app-restful/account.management/controllers/accounts.go
@@ -1,7 +1,11 @@
 package controllers
 
 import (
+	"net/http"
+
 	restful "github.com/emicklei/go-restful"
+	"github.com/nvellon/hal"
+	"github.com/sammyne/ppp-ddd/chapter13/app-restful/conf"
 )
 
 const (
@@ -10,19 +14,19 @@ const (
 )
 
 func Index(req *restful.Request, resp *restful.Response) {
-	resp.Header().Set("Content-Type", "application/hal+json")
+	root := hal.NewResource(struct{}{}, accountsBaseURL)
+	root.AddLink("alternative", hal.NewLink(accountsBaseURL+"accounts?page=1"))
+	root.AddLink("account", hal.NewLink(accountsBaseURL+"accounts/{accountID}"))
+	root.AddLink("account", hal.NewLink(accountsBaseURL+"accounts/123"))
+	root.AddLink("account", hal.NewLink(accountsBaseURL+"accounts/456"))
+	root.AddLink("next", hal.NewLink(accountsBaseURL+"accounts?page=2"))
+	root.AddLink("parent", hal.NewLink(entryPointBaseURL))
 
-	/*
-		root := hal.NewResource(struct{}{}, entryPointBaseURL+"/accountmanagement")
-		root.AddLink("accounts", hal.NewLink(accountsBaseURL+"/accountmanagement/accounts"))
-
-		data, _ := json.Marshal(root)
-		resp.Write(data)
-	*/
+	resp.WriteHeaderAndJson(http.StatusOK, root, "application/hal+json")
 }
 
 func Account(req *restful.Request, resp *restful.Response) {
-	resp.Header().Set("Content-Type", "application/hal+json")
+	resp.Header().Set("Content-Type", conf.MIME_HAL_JSON)
 
 	/*
 		root := hal.NewResource(struct{}{}, entryPointBaseURL+"/accountmanagement")
